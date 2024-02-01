f893a03d1f1d95d3419836fdd17b536fcc380d67
shivi9393 <146976694+shivi9393@users.noreply.github.com>
2024-02-01 10:54:30 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main ()
+{
+    char ch ;
+    int av;
+    printf("enter a character: ");
+    scanf("%c",&ch);
+    av=ch;
+     printf("ascii value %c : %d \n ", ch,av );
+     if (av>=65 && av <=90){
+        printf(" %c is a capital  ");
+          }
+ else if (av>=97 && av <=122 ){
+    printf(" %c is small ");
+ }
+ else if(av>=48 && av<=57){
+   printf("%c is number");
+
+ }
+ else if (( av >=0&&av<=47)||(av>=58 && av>=64)||(av>=91&&av<=96)||(av>=123&&av<=127)){
+   printf("%c special character");
+ }
+
+
+    
+}
